9c1e93959cf42264fa06c2bb4b33def9ac5f6467
AdamPershyi <en0tadam@gmail.com>
2023-09-24 11:39:13 +0300

    syka

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,9 @@ public class Main {
         System.out.println("HelloHelloHello");
         System.out.println("HelloHelloHello");
         System.out.println("HelloHelloHello");
+        System.out.println("HelloHelloHello");
+        System.out.println("HelloHelloHello");
+        System.out.println("HelloHelloHello");
 
 
 
